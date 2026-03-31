2f67015c1921243bae41120c81c003cd7d0fde54
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-30 20:55:19 -0600

    se creo un archivo que sume y reste 2 numeros

---
+++ suma y resta.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+//funcion que sume 2 valores 
+int suma(int num1, int num2){
+    int resultado = 0;
+    resultado=num1+num2;
+    return resultado;
+}
+
+int resta(int num1, int num2){
+    int resultado = 0;
+    resultado=num1-num2;
+    return resultado;
+}
+
+main(){
+
+    cout<<suma(10,20)<<endl;
+    cout<<resta(50,70)<<endl;
+    system("pause");
+}
