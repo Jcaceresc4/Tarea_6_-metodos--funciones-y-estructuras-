401bdffbe349fe5a41532ce3b8d8826c9bcb55a9
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-30 22:42:27 -0600

    se creo una estructura con punteros

---
+++ estructura con punteros.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+using namespace std;
+struct Estudiante{
+    int *codigo;
+    string *nombre;
+    int **notas;
+
+};
+main(){
+    Estudiante estudiante;
+
+    int fila=0,columna =0;
+    cout<<"cuantos estudiantes desea agregar: ";
+    cin>>fila;
+    cout<<"cuantas notas por estudiante desea agregar: ";
+    cin>>columna;
+
+
+    estudiante.codigo=new int[fila];
+    estudiante.nombre=new string[fila];
+    estudiante.notas=new int*[fila];
+    for(int i=0;i<fila;i++){
+        estudiante.notas[i] = new int[columna];
+    }
+    cout<<"____________________Ingreso de Notas____________________________"<<endl;
+    for(int i=0;i<fila;i++){
+        cout<<"Codigo["<<i<<"]: ";
+        cin>>estudiante.codigo[i];
+        cin.ignore();
+        cout<<"Nombre completo["<<i<<"]: ";
+        getline(cin,estudiante.nombre[i]);
+        for (int ii=0;ii<columna;ii++){
+            cout<<"Ingrese Nota["<<ii<<"]: ";
+            cin>>*(*(estudiante.notas+i)+ii);
+        }
+        cout<<"______________________________________"<<endl;
+    }
+    cout<<"______________________Mostrar datos____________________"<<endl;
+    for(int i=0;i<fila;i++){
+        cout<<"Codigo ["<<i<<"]: "<<estudiante.codigo[i]<<endl;
+        cout<<"Nombre Completo["<<i<<"]: "<<estudiante.nombre[i]<<endl;
+        for(int ii=0;ii<fila;ii++){
+            cout<<"Ingrese nota["<<ii<<"]: "<<*(*(estudiante.notas+i)+ii)<<endl;
+        }
+        cout<<"_______________________________________"<<endl;
+    }
+    for (int i=0;i<fila;i++){
+        delete[] estudiante.notas[i];
+    }   
+        delete[] estudiante.codigo;
+        delete[] estudiante.nombre;
+        delete[] estudiante.notas;
+    system("pause");
+}
