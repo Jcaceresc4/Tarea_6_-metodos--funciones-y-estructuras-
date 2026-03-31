551444b37dfe87e0570ad9284e76c51b5f737811
Jcaceresc4 <jcaceresc4@miumg.edu.gt>
2026-03-30 20:47:41 -0600

    se creo un archivo para una suma

---
+++ Metodos y Funciones.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+//funcion que sume 2 valores 
+int suma(int num1, int num2){
+    int resultado = 0;
+    resultado=num1+num2;
+    return resultado;
+}
+
+main(){
+
+    cout<<suma(10,20)<<endl;
+    cout<<suma(50,70)<<endl;
+    cout<<suma(90,10)<<endl;
+    system("pause");
+}
